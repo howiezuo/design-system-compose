3203abd655284c42550730ca962239f039fdda26
Howie Zuo <howie.zuo@gmail.com>
2020-09-15 15:32:53 +0900

    update colors

--- app/src/main/java/io/github/howiezuo/designsystem/compose/ui/DlsColor.kt
+++ app/src/main/java/io/github/howiezuo/designsystem/compose/ui/DlsColor.kt
@@ -6,16 +6,18 @@ import androidx.ui.material.ColorPalette
 import androidx.ui.material.darkColorPalette
 import androidx.ui.material.lightColorPalette
 
-val primary = Color(0xFF3366FF)
-val background = Color(0xFFFFFFFF)
-val backgroundReverse = Color(0xFF192038)
-val basic = Color(0xFF8F9BB3)
-val text = Color(0xFF192038)
-val textReverse = Color(0xFFFFFFFF)
-val success = Color(0xFF00E096)
-val link = Color(0xFF0095FF)
-val warning = Color(0xFFFFAA00)
-val error = Color(0xFFFF3D71)
+object DlsColors {
+    val primary = Color(0xFF3366FF)
+    val background = Color(0xFFFFFFFF)
+    val backgroundReverse = Color(0xFF192038)
+    val basic = Color(0xFF8F9BB3)
+    val text = Color(0xFF192038)
+    val textReverse = Color(0xFFFFFFFF)
+    val success = Color(0xFF00E096)
+    val link = Color(0xFF0095FF)
+    val warning = Color(0xFFFFAA00)
+    val error = Color(0xFFFF3D71)
+}
 
 interface DlsColorPalette {
     val primary: Color
@@ -31,54 +33,40 @@ interface DlsColorPalette {
     val materialColors: ColorPalette
 }
 
-private class DlsObservableColorPalette(
-    override val primary: Color,
-    override val background: Color,
-    override val basic: Color,
-    override val text: Color,
-    override val textReverse: Color,
-    override val success: Color,
-    override val link: Color,
-    override val warning: Color,
-    override val error: Color,
+fun dlsLightColorPalette(): DlsColorPalette = object : DlsColorPalette {
+    override val primary: Color = DlsColors.primary
+    override val background: Color = DlsColors.background
+    override val basic: Color = DlsColors.basic
+    override val text: Color = DlsColors.text
+    override val textReverse: Color = DlsColors.textReverse
+    override val success: Color = DlsColors.success
+    override val link: Color = DlsColors.link
+    override val warning: Color = DlsColors.warning
+    override val error: Color = DlsColors.error
 
-    override val materialColors: ColorPalette
-) : DlsColorPalette
-
-fun dlsLightColorPalette(): DlsColorPalette = DlsObservableColorPalette(
-    primary = primary,
-    background = background,
-    basic = basic,
-    text = text,
-    textReverse = textReverse,
-    success = success,
-    link = link,
-    warning = warning,
-    error = error,
-
-    materialColors = lightColorPalette(
-        primary = primary,
-        surface = background,
-        onSurface = text
+    override val materialColors: ColorPalette = lightColorPalette(
+        primary = DlsColors.primary,
+        surface = DlsColors.backgroundReverse,
+        onSurface = DlsColors.textReverse
     )
-)
+}
 
-fun dlsDarkColorPalette(): DlsColorPalette = DlsObservableColorPalette(
-    primary = primary,
-    background = backgroundReverse,
-    basic = basic,
-    text = textReverse,
-    textReverse = text,
-    success = success,
-    link = link,
-    warning = warning,
-    error = error,
+fun dlsDarkColorPalette(): DlsColorPalette = object : DlsColorPalette {
+    override val primary: Color = DlsColors.primary
+    override val background: Color = DlsColors.backgroundReverse
+    override val basic: Color = DlsColors.basic
+    override val text: Color = DlsColors.textReverse
+    override val textReverse: Color = DlsColors.text
+    override val success: Color = DlsColors.success
+    override val link: Color = DlsColors.link
+    override val warning: Color = DlsColors.warning
+    override val error: Color = DlsColors.error
 
-    materialColors = darkColorPalette(
-        primary = primary,
-        surface = backgroundReverse,
-        onSurface = textReverse
+    override val materialColors: ColorPalette = darkColorPalette(
+        primary = DlsColors.primary,
+        surface = DlsColors.background,
+        onSurface = DlsColors.textReverse
     )
-)
+}
 
 internal val DlsColorAmbient = staticAmbientOf { dlsLightColorPalette() }
